469be78bc9710d84428cbb1dcaf4c2c867ba4eae
MatheusMaximo15 <74806715+MatheusMaximo15@users.noreply.github.com>
2021-02-17 20:20:31 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,66 @@
+const help = (prefix) => {
+	return `
+
+ *PREFIXO: "."*
+       • ──── ✾ ──── •
+       *FIGURINHAS*【✔】
+       • ──── ✾ ──── •
+      
+➸ Comando : *${prefix}sticker* ou *${prefix}stiker*
+➸ útil em : converter imagem/gif/vídeo em adesivo
+➸ uso : responder imagem/gif/video ou enviar imagem/gif/video com legenda\n
+➸ uso : responder imagem ou enviar imagem com legenda/n
+➸ Comando : *${prefix}toimg*
+➸ útil em : converter adesivo em imagem
+➸ uso : adesivo de resposta\n
+
+       • ──── ✾ ──── •
+       *OUTROS...*【✔】
+       • ──── ✾ ──── •
+      
+➸ Comando : *${prefix}wait*
+➸ útil em : pesquisar sobre o anime por imagem [ Que anime é este/que ]
+➸ uso : responder imagem ou enviar imagem com legenda\n
+➸ Comando : *${prefix}setprefix*
+➸ útil em : alterar o prefixo do bot
+➸ uso : *${prefix}setprefix [texto|opcional]*\nexemplo : *${prefix}setprefix ?*
+➸ Nota : Usado somente pelo proprietário do bot\n
+
+       • ─── ✾ ─── •
+       *GRUPO*【✔】
+       • ─── ✾ ─── •
+      
+➸ Comando : *${prefix}linkgroup*
+➸ útil em : enviar o link do grupo
+➸ uso : basta enviar o comando\n
+➸ Comando : *${prefix}marcar*
+➸ útil em : marcar todos os membros do grupo, incluindo administradores
+➸ uso : basta enviar o comando\n
+➸ Nota : Você precisa ser administrador do grupo\n
+➸ Comando : *${prefix}add*
+➸ útil em : adicionar membro ao grupo
+➸ uso : *${prefix}add 5585xxxxx*\n
+➸ Nota : o bot precisa ser admin!\n
+➸ Comando : *${prefix}kick*
+➸ útil em : remover membros do grupo
+➸ uso : *${prefix}kick e o @da pessoa*\n
+➸ Nota : Você precisa ser admin e o bot também
+➸ Comando : *${prefix}promote*
+➸ útil em : tornar membro do grupo um administrador
+➸ uso : *${prefix}promote e o @da pessoa*\n
+➸ Nota : Você precisa ser admin e o bot também
+➸ Comando : *${prefix}demote*
+➸ útil em : tornar o administrador um membro comum
+➸ uso : *${prefix}demote e o @da pessoa*\n
+➸ Nota : Você precisa ser admin e o bot também
+
+`
+}
+
+exports.help = help
+
+
+
+
+
+
